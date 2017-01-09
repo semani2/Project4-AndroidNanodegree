4bdefcf3914b176c4e88d0cb697aa633a43f2aa4
Sai Prajnan Emani <sai.emani@citrix.com>
2017-01-09 16:05:06 -0500

    Android test to test functioning of asynctask implemented.

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTaskTest.java
@@ -0,0 +1,52 @@
+package com.udacity.gradle.builditbigger;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.udacity.gradle.builditbigger.asynctasks.EndpointsAsyncTask;
+import com.udacity.gradle.builditbigger.ui.MainActivity;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by sai on 1/9/17.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class AsyncTaskTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+
+    private static String response = null;
+
+    @Test
+    public void demoTest() throws Exception {
+        final MainActivity mainActivity = activityRule.getActivity();
+
+        final CountDownLatch signal = new CountDownLatch(1);
+
+        mainActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                new EndpointsAsyncTask(){
+                    @Override
+                    protected void onPostExecute(String result) {
+                        super.onPostExecute(result);
+                        response = result;
+                        signal.countDown();
+                    }
+                }.execute(mainActivity);
+            }
+        });
+
+        signal.await(10, TimeUnit.SECONDS);
+        Assert.assertNotNull(response);
+    }
+}
